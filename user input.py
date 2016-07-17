452ff15c7e4c8861a216209644ff47db91c9fbde
achrafcsa <achraf.atila@gmail.com>
2016-07-17 16:47:46 +0100

    Add files via upload

---
+++ user input.py
@@ -0,0 +1,10 @@
+#name = input("whats your name? ")
+
+#print("Hello " + name)
+
+x = float(input("please enter a number: "))
+y = float(input("please enter a number: "))
+
+var = x + y
+
+print(var)
